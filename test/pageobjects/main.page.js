afed50b873ff882992bcd0c0ab8ef42f6ecdeab0
EugeneShut <admin@jeka.pw>
2021-12-21 20:30:07 +0300

    add working post js method

--- test/pageobjects/main.page.js
+++ test/pageobjects/main.page.js
@@ -1,9 +1,29 @@
-
-
 const Page = require('./page');
+var request = require('request');
 
 
 class MainPage extends Page {
+    get post() {
+        var options = {
+            'method': 'POST',
+            'url': 'https://api.vk.com/method/wall.post',
+            'headers': {
+            },
+            formData: {
+                'access_token': 'b7322768e78e375e4e9a4fd623c852b7efb4435072abea51d7fa8798181b80d0cb31fe57a259052be8d14',
+                'message': 'teist',
+                'v': '5.131'
+            }
+        };
+        request(options, function (error, response) {
+            if (error) throw new Error(error);
+            console.log(response.body);
+        });
+    }
+
+    get inputPassword() {
+        return $('#index_pass');
+    }
 
     open() {
         return $('#l_pr').click();
--- test/specs/test.vk.js
+++ test/specs/test.vk.js
@@ -2,14 +2,18 @@ const LoginPage = require('../pageobjects/login.page');
 const MainPage = require('../pageobjects/main.page');
 
 
+
 describe('VK Test', () => {
     it('vk', async () => {
-        await LoginPage.open();
-        await browser.pause(2000)
-        await LoginPage.login('+375291660762', 'PuV6j_.2&$m9h?UYY');
-        await browser.pause(2000)
-        await MainPage.open()
-        await browser.pause(2000)
+        await MainPage.post;
+        // await LoginPage.open();
+        // await browser.pause(2000)
+        // await LoginPage.login('+375291660762', 'PuV6j_.2&$m9h?UYY');
+        // await browser.pause(2000)
+        // await MainPage.open()
+        // await browser.pause(2000)
+
+        // await request.get("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
         //await expect(SecurePage.flashAlert).toBeExisting();
         //await expect(SecurePage.flashAlert).toHaveTextContaining(
         //    'You logged into a secure area!');
