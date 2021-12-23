418dcba84c85949d78339fb0371b69db8c908c1a
EugeneShut <admin@jeka.pw>
2021-12-23 16:31:28 +0300

    refactor x3

--- test/specs/test.vk.js
+++ test/specs/test.vk.js
@@ -1,6 +1,5 @@
 const LoginPage = require('../pageobjects/login.page');
 const MainPage = require('../pageobjects/main.page');
-import VkApi from "../API/api";
 const expectChai = require('chai').expect;
 
 
@@ -10,16 +9,17 @@ describe('VK Test', () => {
         await LoginPage.open()
         await LoginPage.login('+375291660762', 'PuV6j_.2&$m9h?UYY') // TODO: HIDE CREDENTIALS
         await MainPage.open()
-        let post_id = await MainPage.wallRandomPost()
 
-        await browser.pause(4000)
+        let post_id = await MainPage.wallRandomPost()
+        await browser.pause(4000) // TODO: change to intercept
+
         let last_post = await MainPage.LastPostId()
         await expectChai(post_id).to.equal(last_post)
 
         let image = await MainPage.wallPostImage("627657327", "images/image.png")
         await MainPage.wallPostEdit(post_id, image)
 
-        await browser.pause(4000)
+        await browser.pause(4000) // TODO: remove
 
     });
 });
