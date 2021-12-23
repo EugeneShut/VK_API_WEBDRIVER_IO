5d16b213d488d6bfb1ede4d6e7330b9161e912b8
EugeneShut <admin@jeka.pw>
2021-12-23 16:28:23 +0300

    refactor x2

--- test/pageobjects/main.page.js
+++ test/pageobjects/main.page.js
@@ -1,9 +1,37 @@
+import VkApi from "../API/api";
+
 const Page = require('./page');
 
 
 class MainPage extends Page {
     get lastPost() {
         return $('#page_wall_posts > div').getAttribute("id")
+    }
+
+    async wallRandomPost() {
+        let post_id
+        return await browser.call(() => {
+            return VkApi.PostRandomWallPost()
+                .then(data => post_id = data)
+                .catch(err => console.log(err))
+        })
+    }
+
+    async wallPostImage(user_id, image_path) {
+        let image
+        return await browser.call(() => {
+            return VkApi.PostImageToWall(user_id, image_path) // TODO: change userID and PATH
+                .then(data => image = data)
+                .catch(err => console.log(err))
+        })
+    }
+
+    async wallPostEdit(post_id, image="") {
+        return await browser.call(() => {
+            return VkApi.EditWallPost(post_id, image,"aqa: edited post")
+                .then(data => post_id = data)
+                .catch(err => console.log(err))
+        })
     }
 
     async LastPostId() {
--- test/specs/test.vk.js
+++ test/specs/test.vk.js
@@ -5,34 +5,20 @@ const expectChai = require('chai').expect;
 
 
 describe('VK Test', () => {
-    let post_id
+
     it('vk', async () => {
         await LoginPage.open()
         await LoginPage.login('+375291660762', 'PuV6j_.2&$m9h?UYY') // TODO: HIDE CREDENTIALS
         await MainPage.open()
-        await browser.call(() => {
-            return VkApi.PostRandomWallPost()
-                .then(data => post_id = data)
-                .catch(err => console.log(err))
-        })
-        await console.log(post_id)
+        let post_id = await MainPage.wallRandomPost()
+
         await browser.pause(4000)
         let last_post = await MainPage.LastPostId()
         await expectChai(post_id).to.equal(last_post)
 
-        let image
-        await browser.call(() => {
-            return VkApi.PostImageToWall("627657327", "images/image.png") // TODO: change userID and PATH
-                .then(data => image = data)
-                .catch(err => console.log(err))
-        })
+        let image = await MainPage.wallPostImage("627657327", "images/image.png")
+        await MainPage.wallPostEdit(post_id, image)
 
-
-        await browser.call(() => {
-            return VkApi.EditWallPost(post_id, image,"aqa: edited post")
-                .then(data => post_id = data)
-                .catch(err => console.log(err))
-        })
         await browser.pause(4000)
 
     });
