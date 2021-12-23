3e1ef4582ab999392388e5675b732ebde74e89c6
EugeneShut <admin@jeka.pw>
2021-12-23 15:34:31 +0300

    photo send is working

--- test/API/api.js
+++ test/API/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const helper = require('../../helpers/helper')
 const FormData = require('form-data');
+const fs = require('fs');
 
 const API_BASE_URL = "https://api.vk.com";
 const API_TOKEN = "c2ad3c985319a3660968f2ed11f891e0577740e7356ba1bf793b296c8161fd4fdbe0d951da623335a04f3"
@@ -29,6 +30,75 @@ class VkApi {
         } catch(err) {
             return err;
         }
+    }
+
+    async getUploadLink() {
+        let data_d = new FormData();
+        data_d.append('access_token', API_TOKEN);
+        data_d.append('v', API_VK_VERSION);
+
+        let config = {
+            method: 'post',
+            url: API_BASE_URL + "/method/photos.getWallUploadServer",
+            headers: {
+                ...data_d.getHeaders()
+            },
+            data: data_d
+        }
+        let response = await axios(config)
+        console.log(response.data.response.upload_url)
+        return  response.data.response.upload_url
+    }
+
+    async UploadImage(image_path) {
+        try {
+            let link = await this.getUploadLink()
+            var imageData = new FormData()
+            const path = require('path');
+            const coolPath = path.join(__dirname, '..', '..', 'images/image.png')
+            // const image_path = fs.createReadStream("image.png")
+            // image_path.on('data', function (chunk) {
+            //     console.log(chunk.toString())
+            // })
+            imageData.append('photo', fs.createReadStream('image.png'));
+            let push_image_config = {
+                method: 'post',
+                url: link,
+                headers: {
+                    'Cookie': 'remixlang=114',
+                    ...imageData.getHeaders()
+                },
+                data: imageData,
+            }
+            let push_image = await axios(push_image_config)
+            return push_image.data
+
+        } catch(err) {
+            return err;
+        }
+    }
+
+    async SaveImage(user_id, push_image) {
+        let data_d = new FormData();
+        data_d.append('access_token', API_TOKEN);
+        data_d.append('v', API_VK_VERSION);
+        data_d.append("user_id", user_id)
+        data_d.append("photo", push_image.data.photo)
+        data_d.append("server", push_image.data.server)
+        data_d.append("hash", push_image.data.hash)
+
+
+        let save_image_comfig = {
+            method: 'post',
+            url: API_BASE_URL + "/method/photos.saveWallPhoto",
+            headers: {
+                ...data_d.getHeaders()
+            },
+            data: data_d
+        }
+        let result = await axios(save_image_comfig)
+        console.log(data_d)
+        return result.data.response
     }
 
     async EditWallPost(post_id, image="", message="") {
--- test/specs/test.vk.js
+++ test/specs/test.vk.js
@@ -7,25 +7,41 @@ const expectChai = require('chai').expect;
 
 describe('VK Test', () => {
     it('vk', async () => {
-        await LoginPage.open();
-        await LoginPage.login('+375291660762', 'PuV6j_.2&$m9h?UYY');
-        await MainPage.open()
+        // await LoginPage.open();
+        // await LoginPage.login('+375291660762', 'PuV6j_.2&$m9h?UYY');
+        // await MainPage.open()
         let post_id;
+        // await browser.call(() => {
+        //     return VkApi.PostRandomWallPost()
+        //         .then(data => post_id = data)
+        //         .catch(err => console.log(err))
+        // })
+        // await console.log(post_id);
+        // await browser.pause(4000)
+        // let posted_id = await MainPage.LastPostId()
+        // await expectChai(post_id).to.equal(posted_id)
+        // await browser.call(() => {
+        //     return VkApi.EditWallPost(post_id, "","edited post")
+        //         .then(data => post_id = data)
+        //         .catch(err => console.log(err))
+        // })
+        // await browser.pause(4000)
+        let push_data = "________________________________";
         await browser.call(() => {
-            return VkApi.PostRandomWallPost()
-                .then(data => post_id = data)
+            return VkApi.UploadImage("images/image.png")
+                .then(data => push_data = data)
                 .catch(err => console.log(err))
         })
-        await console.log(post_id);
-        await browser.pause(4000)
-        let posted_id = await MainPage.LastPostId()
-        await expectChai(post_id).to.equal(posted_id)
-        await browser.call(() => {
-            return VkApi.EditWallPost(post_id, "","edited post")
-                .then(data => post_id = data)
-                .catch(err => console.log(err))
-        })
-        await browser.pause(4000)
+        console.log(push_data)
+
+        // let link;
+        // await browser.call(() => {
+        //     return VkApi.SaveImage("457245425", push_data)
+        //         .then(data => link = data)
+        //         .catch(err => console.log(err))
+        // })
+        // // "457245425"
+        // console.log(link)
 
     });
 });
