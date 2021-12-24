e4498aac413fa0d8dde24ad8d926857915fa0173
EugeneShut <admin@jeka.pw>
2021-12-24 13:52:16 +0300

    add a config

--- test/specs/test.vk.js
+++ test/specs/test.vk.js
@@ -1,13 +1,13 @@
 const LoginPage = require('../pageobjects/login.page');
 const MainPage = require('../pageobjects/main.page');
 const expectChai = require('chai').expect;
-
+var config = require('../../config')
 
 describe('VK Test', () => {
 
     it('vk', async () => {
         await LoginPage.open()
-        await LoginPage.login('+375291660762', 'PuV6j_.2&$m9h?UYY') // TODO: HIDE CREDENTIALS
+        await LoginPage.login(config.credentials.login, config.credentials.password) // TODO: HIDE CREDENTIALS
         await MainPage.open()
         const user_id = await MainPage.GetUserId()
 
@@ -16,7 +16,7 @@ describe('VK Test', () => {
         let last_post = await MainPage.LastPostId(user_id, post_id)
         await expectChai(post_id).to.equal(last_post)
 
-        let image = await MainPage.wallPostImage(user_id, "images/image.png")
+        let image = await MainPage.wallPostImage(user_id, config.image)
         await MainPage.wallPostEdit(post_id, image)
 
         await browser.pause(4000) // TODO: remove
