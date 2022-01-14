66984e2882e6c7d10a87a96c186634b39ffa2e32
EugeneShut <admin@jeka.pw>
2022-01-14 16:16:55 +0300

    add baseApi class

--- test/API/api.js
+++ test/API/api.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import BaseApi from "../API/BaseApi";
 const helper = require('../../helpers/helper')
 const FormData = require('form-data');
 const fs = require('fs');
@@ -33,26 +33,20 @@ class VkApi {
     likesGetList = "/method/likes.getList"
     wallDelete = "/method/wall.delete"
 
+
     async getUserId() {
         try {
             let data_d = new FormData();
             data_d.append(ACCESS_TOKEN, API_TOKEN);
             data_d.append(V, API_VK_VERSION);
 
-            let config = {
-                method: POST,
-                url: API_BASE_URL + this.usersGet,
-                headers: {
-                    ...data_d.getHeaders()
-                },
-                data: data_d
-            }
-            let response = await axios(config)
+            let response = await BaseApi.postRequest(API_BASE_URL + this.usersGet, data_d)
             return response.data.response[0].id.toString();
         } catch(err) {
             return err;
         }
     }
+
 
     async postRandomWallPost() {
         try {
@@ -61,15 +55,7 @@ class VkApi {
             data_d.append(MESSAGE, helper.generate_string());
             data_d.append(V, API_VK_VERSION);
 
-            let config = {
-                method: POST,
-                url: API_BASE_URL + this.wallPost,
-                headers: {
-                    ...data_d.getHeaders()
-                },
-                data: data_d
-            }
-            let response = await axios(config)
+            let response = await BaseApi.postRequest(API_BASE_URL + this.wallPost, data_d)
             return response.data.response.post_id.toString();
         } catch(err) {
             return err;
@@ -81,35 +67,19 @@ class VkApi {
         data_d.append(ACCESS_TOKEN, API_TOKEN);
         data_d.append(V, API_VK_VERSION);
 
-        let config = {
-            method: POST,
-            url: API_BASE_URL + this.getWallUploadServer,
-            headers: {
-                ...data_d.getHeaders()
-            },
-            data: data_d
-        }
-        let response = await axios(config)
+        let response = await BaseApi.postRequest(API_BASE_URL + this.getWallUploadServer, data_d)
         return  response.data.response.upload_url
     }
 
-    async uploadImage(image_path) {
+    async uploadImage(image_path) { // todo: debug!!
         try {
             let link = await this.getUploadLink()
-            var imageData = new FormData()
+            let imageData = new FormData()
             const path = require('path');
             const coolPath = path.join(__dirname, '..', '..', image_path)
             imageData.append(PHOTO, fs.createReadStream(coolPath));
-            let push_image_config = {
-                method: POST,
-                url: link,
-                headers: {
-                    'Cookie': 'remixlang=114',
-                    ...imageData.getHeaders()
-                },
-                data: imageData,
-            }
-            let push_image = await axios(push_image_config)
+
+            let push_image = await BaseApi.postRequest(link, imageData, {'Cookie': 'remixlang=114'}) // todo HEADER
             return push_image.data
 
         } catch(err) {
@@ -127,16 +97,7 @@ class VkApi {
         data_d.append(HASH, push_image.hash)
         data_d.append(PHOTO, push_image.photo)
 
-
-        let save_image_comfig = {
-            method: POST,
-            url: API_BASE_URL + this.saveWallPhoto,
-            headers: {
-                ...data_d.getHeaders()
-            },
-            data: data_d
-        }
-        let result = await axios(save_image_comfig)
+        let result = await BaseApi.postRequest(API_BASE_URL + this.saveWallPhoto, data_d)
         return PHOTO + user_id + "_" + result.data.response[0].id
     }
 
@@ -149,15 +110,7 @@ class VkApi {
             data_d.append(ATTACHMENTS, image);
             data_d.append(MESSAGE, message);
 
-            let config = {
-                method: POST,
-                url: API_BASE_URL + this.wallEdit,
-                headers: {
-                    ...data_d.getHeaders()
-                },
-                data: data_d
-            }
-            let response = await axios(config)
+            let response = await BaseApi.postRequest(API_BASE_URL + this.wallEdit, data_d)
             return response.data.response.post_id.toString();
         } catch(err) {
             return err;
@@ -172,15 +125,7 @@ class VkApi {
             data_d.append(POST_ID, post_id);
             data_d.append(MESSAGE, message);
 
-            let config = {
-                method: POST,
-                url: API_BASE_URL + this.wallCreateComment,
-                headers: {
-                    ...data_d.getHeaders()
-                },
-                data: data_d
-            }
-            let response = await axios(config)
+            let response = await BaseApi.postRequest(API_BASE_URL + this.wallCreateComment, data_d)
             return response.data.response.post_id.toString();
         } catch(err) {
             return err;
@@ -195,15 +140,7 @@ class VkApi {
             data_d.append(ITEM_ID, post_id);
             data_d.append(TYPE, POST);
 
-            let config = {
-                method: POST,
-                url: API_BASE_URL + this.likesGetList,
-                headers: {
-                    ...data_d.getHeaders()
-                },
-                data: data_d
-            }
-            let response = await axios(config)
+            let response = await BaseApi.postRequest(API_BASE_URL + this.likesGetList, data_d)
             return response.data.response.items[0].toString();
         } catch(err) {
             return err;
@@ -217,15 +154,7 @@ class VkApi {
             data_d.append(V, API_VK_VERSION);
             data_d.append(POST_ID, post_id);
 
-            let config = {
-                method: POST,
-                url: API_BASE_URL + this.wallDelete,
-                headers: {
-                    ...data_d.getHeaders()
-                },
-                data: data_d
-            }
-            let response = await axios(config)
+            let response = await BaseApi.postRequest(API_BASE_URL + this.wallDelete, data_d)
             return response.data;
         } catch(err) {
             return err;
